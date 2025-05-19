4a0f98c3eaf57fef2077c64d0bd27f72b7b553c0
Kyle Spence <kyle.w.spence@gmail.com>
2025-05-19 20:31:46 +0000

    completed and tested palindrome exercise

--- practice/01-basic-challenges-1/08-palindrome/palindrome.ts
+++ practice/01-basic-challenges-1/08-palindrome/palindrome.ts
@@ -1,3 +1,9 @@
 export function isPalindrome(str: string): boolean {
-  return false;
+  console.log('running');
+  const formattedStr = str.toLowerCase().replace(/[^a-z0-9]/g, '');
+  console.log(formattedStr);
+  const reversedStr = formattedStr.split('').reverse().join('');
+  return formattedStr === reversedStr;
 }
+
+console.log(isPalindrome('abc\\123'));
