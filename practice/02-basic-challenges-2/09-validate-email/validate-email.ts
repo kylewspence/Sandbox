59a08660d4680f9478b66c20f99666dd49614667
Kyle Spence <kyle.w.spence@gmail.com>
2025-05-30 23:10:11 +0000

    validate email - regex w/ help

--- practice/02-basic-challenges-2/09-validate-email/validate-email.ts
+++ practice/02-basic-challenges-2/09-validate-email/validate-email.ts
@@ -1,3 +1,4 @@
 export function validateEmail(email: string): boolean {
-  return false;
+  const emailRegex = /^[A-Za-z0-9._%+-]+@[A-Za-z0-9._%+-]+\.[A-Za-z]{2,}$/;
+  return emailRegex.test(email);
 }
