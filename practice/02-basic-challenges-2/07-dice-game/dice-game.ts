9801e4dbd48c6cd0dc4b2c650e0e2e68a48f9903
Kyle Spence <kyle.w.spence@gmail.com>
2025-05-30 21:30:16 +0000

    dice game

--- practice/02-basic-challenges-2/07-dice-game/dice-game.ts
+++ practice/02-basic-challenges-2/07-dice-game/dice-game.ts
@@ -7,5 +7,20 @@ export type Simulation = {
 };
 
 export function diceGameSimulation(numSimulations: number): Simulation[] {
-  return [];
+  const results = [];
+  for (let i = 0; i < numSimulations; i++) {
+    const dice1 = (Math.floor(Math.random() * 6) + 1) as DiceRoll;
+    const dice2 = (Math.floor(Math.random() * 6) + 1) as DiceRoll;
+    const sum = dice1 + dice2;
+    let result: Simulation['result'];
+    if (sum === 7 || sum === 11) {
+      result = 'win';
+    } else if (sum === 2 || sum === 3 || sum === 12) {
+      result = 'lose';
+    } else {
+      result = 'roll again';
+    }
+    results.push({ dice1, dice2, sum, result });
+  }
+  return results;
 }
