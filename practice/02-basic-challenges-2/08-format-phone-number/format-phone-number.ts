39edc8de1a2657f0f407b0324c75bf4f5c20df3f
Kyle Spence <kyle.w.spence@gmail.com>
2025-05-30 22:50:33 +0000

    format-phone-number no help

--- practice/02-basic-challenges-2/08-format-phone-number/format-phone-number.ts
+++ practice/02-basic-challenges-2/08-format-phone-number/format-phone-number.ts
@@ -1,3 +1,7 @@
 export function formatPhoneNumber(numbers: number[]): string {
-  return '';
+  const areaCode = numbers.slice(0, 3).join('');
+  const firstThree = numbers.slice(3, 6).join('');
+  const lastFour = numbers.slice(6, 10).join('');
+  const phoneNumber = `(${areaCode}) ${firstThree}-${lastFour}`;
+  return phoneNumber;
 }
