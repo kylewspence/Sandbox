bb818ff129f3a1f7af8c20848ba199e8ec7d82d0
Kyle Spence <kyle.w.spence@gmail.com>
2025-05-30 01:27:33 +0000

    cleaned up missing numbers

--- practice/02-basic-challenges-2/03-find-missing-number/find-missing-number.ts
+++ practice/02-basic-challenges-2/03-find-missing-number/find-missing-number.ts
@@ -5,8 +5,3 @@ export function findMissingNumber(arr: number[]): number | undefined {
   const missing = sum - arrSum;
   return missing;
 }
-
-// Calculate the sum of the numbers from 1 to n (inclusive). The formula for this is n * (n + 1) / 2, where n is the length of the array plus 1.
-// Calculate the sum of the numbers in the array.
-// Subtract the sum of the numbers in the array from the sum of the numbers from 1 to n (inclusive). The difference is the missing number.
-// You could use a for loop or the reduce method to calculate the sum of the numbers in the array. We will be focusing on methods like reduce in the next section, but both solutions are included here.
