04c64ebb8ff3615f7dc61938f607aea643aa2b06
Kyle Spence <kyle.w.spence@gmail.com>
2025-05-30 20:25:20 +0000

    first-non-repeating

--- practice/02-basic-challenges-2/06-first-non-repeating/first-non-repeating.ts
+++ practice/02-basic-challenges-2/06-first-non-repeating/first-non-repeating.ts
@@ -1,5 +1,16 @@
 export function findFirstNonRepeatingCharacter(
   str: string
 ): string | undefined {
+  const charCount: Record<string, number> = {};
+
+  for (const char of str) {
+    charCount[char] = (charCount[char] ?? 0) + 1;
+  }
+
+  for (const char of str) {
+    if (charCount[char] === 1) {
+      return char;
+    }
+  }
   return undefined;
 }
