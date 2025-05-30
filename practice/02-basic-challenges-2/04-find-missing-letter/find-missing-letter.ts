d769cf1fa5fc6dad77666f5208fc2969dfc02e39
Kyle Spence <kyle.w.spence@gmail.com>
2025-05-30 01:20:42 +0000

    find-missing-number

--- practice/02-basic-challenges-2/04-find-missing-letter/find-missing-letter.ts
+++ practice/02-basic-challenges-2/04-find-missing-letter/find-missing-letter.ts
@@ -1,3 +1,8 @@
 export function findMissingLetter(arr: string[]): string | undefined {
-  return undefined;
+  const startCode = arr[0].charCodeAt(0); // number of the first element in the array
+  for (let i = 0; i < arr.length; i++) {
+    if (String.fromCharCode(startCode + i) !== arr[i]) {
+      return String.fromCharCode(startCode + i);
+    }
+  }
 }
