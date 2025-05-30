5e976a6962754e079c111fee34b3b28c699d7555
Kyle Spence <kyle.w.spence@gmail.com>
2025-05-30 23:12:05 +0000

    simple-examples

--- practice/03-high-order-array-methods/01-simple-examples/simple-examples.ts
+++ practice/03-high-order-array-methods/01-simple-examples/simple-examples.ts
@@ -2,18 +2,48 @@ const numbers = [1, 2, 3, 4, 5];
 
 /**
  * map: Transforms array elements with a provided function, creating a new array.
+ * const doubledNumbers = numbers.map((num) => {
+  return num * 2;
+});
+
+console.log(doubledNumbers);
  */
 
 /**
  * filter: Creates a new array with elements that satisfy a specified condition.
+ * const numbers = [1, 2, 3, 4, 5];
+
+const evenNumbers = numbers.filter((num) => {
+  return num % 2 === 0;
+});
+
+console.log(evenNumbers);
+
+// Output: [2, 4]
  */
 
 /**
  * reduce: Accumulates array elements into a single value using a provided function.
+ * const numbers = [1, 2, 3, 4, 5];
+
+const sum = numbers.reduce((total, num) => {
+  return total + num;
+}, 0);
+
+console.log(sum);
+
+// Output: 15
  */
 
 /**
  * forEach: Iterates through array elements and applies a function without creating a new array.
+ * const numbers = [1, 2, 3, 4, 5];
+
+numbers.forEach((num) => {
+  console.log(num);
+});
+
+// Output: 1 2 3 4 5
  */
 
 /**
