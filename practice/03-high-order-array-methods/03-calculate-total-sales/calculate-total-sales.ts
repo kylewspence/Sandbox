1c17258dc21bc891c3dd815a8bd83807f96da440
Kyle Spence <kyle.w.spence@gmail.com>
2025-06-01 20:45:48 +0000

    calculate-total-sales

--- practice/03-high-order-array-methods/03-calculate-total-sales/calculate-total-sales.ts
+++ practice/03-high-order-array-methods/03-calculate-total-sales/calculate-total-sales.ts
@@ -8,5 +8,12 @@ export function calculateTotalSalesWithTax(
   products: Product[],
   taxRate: number
 ): number {
-  return NaN;
+  const totalSales = products.reduce(
+    (sum, product) => sum + product.price * product.quantity,
+    0
+  );
+
+  const taxAmount = (totalSales * taxRate) / 100;
+  const total = totalSales + taxAmount;
+  return parseFloat(total.toFixed(2));
 }
